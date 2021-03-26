0090faa5adfeb4fa0b493d46b726eff2785b9168
jha0402 <67306416+jha0402@users.noreply.github.com>
2021-03-26 18:10:40 +0900

    setup firebase

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
-import { Avatar, IconButton } from '@material-ui/core';
+import { Avatar, Button, IconButton } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import ChatIcon from '@material-ui/icons/Chat';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Sidebar() {
 	return (
@@ -12,10 +13,19 @@ function Sidebar() {
 					<Icons>
 						<IconButton>
 							<ChatIcon />
+						</IconButton>
+						<IconButton>
 							<MoreVertIcon />
 						</IconButton>
 					</Icons>
 				</Head>
+
+				<Search>
+					<SearchIcon />
+					<SearchInput placeholder="채팅 검색" />
+				</Search>
+
+				<ChattingButton>새 채팅 시작하기</ChattingButton>
 			</Container>
 		</div>
 	);
@@ -27,8 +37,45 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Head = styled.div``;
+const Head = styled.div`
+	display: flex;
+	position: sticky;
+	top: 0;
+	height: 80px;
+	padding: 8px;
+	z-index: 1;
+	justify-content: space-between;
+	align-items: center;
+	border-bottom: 1px solid whitesmoke;
+`;
 
 const Icons = styled.div``;
 
-const UserPhoto = styled(Avatar)``;
+const UserPhoto = styled(Avatar)`
+	cursor: pointer;
+	:hover {
+		opacity: 0.7;
+	}
+`;
+
+const Search = styled.div`
+	display: flex;
+	padding: 16px;
+	align-items: center;
+`;
+
+const SearchInput = styled.input`
+	flex: 1;
+	border: none;
+	:focus {
+		outline: none;
+	}
+`;
+
+const ChattingButton = styled(Button)`
+	width: 100%;
+	&&& {
+		border-bottom: 1px solid whitesmoke;
+		border-top: 1px solid whitesmoke;
+	}
+`;
---
+++ firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: 'AIzaSyDwWJkIAYUXe6ypmMM__6QIQvw4jDv9uhc',
+	authDomain: 'hotair2-5e895.firebaseapp.com',
+	projectId: 'hotair2-5e895',
+	storageBucket: 'hotair2-5e895.appspot.com',
+	messagingSenderId: '122296470914',
+	appId: '1:122296470914:web:3e8d827d3ef87582fc939b',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const app = !firebase.apps.length
+	? firebase.initializeApp(firebaseConfig)
+	: firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
